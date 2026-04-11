b21a11b0721cc521363d3e09cd352166cdc4222b
Jonny84-max <jonnybest0684@gmail.com>
2026-04-11 18:28:04 +0100

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -7,7 +7,7 @@ df = pd.read_csv("biomimetic_dataset.csv")
 X = df.drop(columns=["drag_reduction", "biofouling", "hydrophobicity", "durability"])
 y = df[["drag_reduction", "biofouling", "hydrophobicity", "durability"]]
 
-model = RandomForestRegressor(n_estimators=80, random_state=42)
+model = RandomForestRegressor(n_estimators=50, random_state=42)
 model.fit(X, y)
 
 joblib.dump({
