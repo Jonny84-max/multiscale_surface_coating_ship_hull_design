c79dde71ce3c9204635a8f88051f81c15e5ef372
Jonny84-max <jonnybest0684@gmail.com>
2026-04-11 14:56:18 +0100

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestRegressor
+
+df = pd.read_csv("biomimetic_dataset.csv")
+
+X = df.drop(columns=["drag_reduction", "biofouling", "hydrophobicity", "durability"])
+y = df[["drag_reduction", "biofouling", "hydrophobicity", "durability"]]
+
+model = RandomForestRegressor(n_estimators=150, random_state=42)
+model.fit(X, y)
+
+joblib.dump(model, "model.pkl")
+
+print("Model trained")
